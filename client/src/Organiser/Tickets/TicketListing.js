55f09528b0a035b2d1629abaca46cfd9e5f6014b
Zodiakc <mbinternet@mail.ru>
2023-03-22 09:39:51 +0300

    Change items to 1/4 and TicketListing is on the center

--- client/src/Organiser/Tickets/TicketListing.js
+++ client/src/Organiser/Tickets/TicketListing.js
@@ -2,17 +2,17 @@ import React from "react";
 import Ticket from "./Ticket";
 
 function TicketListing() {
-  return (
-    <div className="container grid grid-cols-3 items-center justify-center">
-      <Ticket />
-      <Ticket />
-      <Ticket />
-      <Ticket />
-      <Ticket />
-      <Ticket />
-      <Ticket />
-    </div>
-  );
+    return (
+        <div className="container mx-auto grid grid-cols-4 items-center justify-center  ">
+            <Ticket />
+            <Ticket />
+            <Ticket />
+            <Ticket />
+            <Ticket />
+            <Ticket />
+            <Ticket />
+        </div>
+    );
 }
 
 export default TicketListing;
